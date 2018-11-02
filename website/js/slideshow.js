5ee84c2218234bf6b6a50e38d4afc6c3572a229f
Riku Kaipainen <shylessfly@gmail.com>
2018-11-02 15:11:44 +0200

    Main page with simple slideshow and introduction done. No login yet.

--- website/js/slideshow.js
+++ website/js/slideshow.js
@@ -1,17 +1,25 @@
-var imgIndex = 1;
-showSlide(imgIndex);
-
-function nextSlide(n) {
-	showSlide(imgIndex += n);
-}
+showSlide(0);
+var next = 1;
+setInterval("nextSlide()", 5000);
 
 function showSlide(n) {
-	var slide = 1;
 	var slides = document.getElementsByClassName("slides");
-	if (n > slides.length) { imgIndex = 1; }
-	if (n < 1) { imgIndex = slides.length; }
-	for (slide = 0; slide < slides.length; slide++) {
+	var dots = document.getElementsByClassName("dot");
+	var newSlide = n + 1;
+	if (n >= slides.length) {	newSlide = 1; n = 0; }
+	else if (n < 0) { newSlide = slides.length - 1; n = slides.length; }
+	for (var slide = 0; slide < slides.length; slide++) {
 		slides[slide].style.display = "none";
+		slides[slide].className = slides[slide].className.replace(" fadeIn", "");
 	}
-	slides[imgIndex - 1].style.display = "block";
+	for (var dot = 0; dot < dots.length; dot++) {
+		dots[dot].className = dots[dot].className.replace(" active", "");
+	}
+	slides[n].style.display = "block"; slides[n].className += " fadeIn";
+	dots[n].className += " active";
+	return newSlide;
+}
+
+function nextSlide() {
+	next = showSlide(next);
 }
