7b9b94a962a014241f43ed17aa1b53fc663552a4
Riku Kaipainen <shylessfly@gmail.com>
2018-11-05 17:34:42 +0200

    Small POST example using express

--- website/server.js
+++ website/server.js
@@ -1,10 +1,43 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
 
+//Serve public folders static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Use bodyparser to parse POST forms (eg. req.body.'input name')
+app.use(bodyparser.urlencoded({extended: false}));
+
+//Use cookie parser to manage cookies
+app.use(cookieParser());
+
+function query(func) {
+	var db = mysql.createConnection({
+		host: "localhost",
+		user: "dbuser",
+		password: "Sepsis123Database",
+		database: "AjoneuvonSeuranta"
+	});
+	db.connect((err) => {
+		if (err) {
+			throw err;
+		}
+		console.log('MySQL Connected!');
+		return func();
+	});
+}
+
+app.post('/validateLogin', (req, res) => {
+	console.log("Trying to login with " + req.body.uname + req.body.psw);
+
+	res.cookie('un', req.body.uname);
+	res.cookie('pw', req.body.psw);
+	res.end();
+});
 
 const server = app.listen(8080, () => {
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, 'src/index.html'));
-	});
+	console.log("Server running at port 8080!");
 });
