f7e639c8a8dd1c95b670113b63971a55d9568936
Karhu12 <shylessfly@gmail.com>
2018-12-04 10:02:26 +0000

    MainPage Ready for merge with list page

--- website/public/js/map.js
+++ website/public/js/map.js
@@ -21,7 +21,7 @@ function placeMarker(unit) {
     });
     google.maps.event.addListener(marker, 'click', function(){
         infoWindow.close();
-        infoWindow.setContent( "<div><h1>Unit "+ unit.device_id +"</h1><h4>Location</h4 ><p>Lat: " + unit.latitude + "<br>Lng: " + unit.longitude + "<br>Temp: " + unit.temp + "</p></div>");
+        infoWindow.setContent( "<div><h1>Unit "+ unit.device_id +"</h1><h4>Location</h4 ><p>Lat: " + unit.latitude + "<br>Lng: " + unit.longitude + "</p><h4>Sensors</h4 ><p>Temperature: " + unit.temp + "°C</p></div>");
         infoWindow.open(map, marker);
     });
     markers.push(marker);
